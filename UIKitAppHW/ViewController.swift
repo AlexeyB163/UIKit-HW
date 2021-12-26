849c5e1b349a214c9a1d0fb5989c72f2e9956498
AlexeyB163 <sourceaisa@mail.ru>
2021-12-27 00:31:00 +0400

    logic completed

--- UIKitAppHW/ViewController.swift
+++ UIKitAppHW/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var viewColor: UIView!
+    @IBOutlet weak var viewLabel: UIView!
     
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
@@ -21,22 +21,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewColor.layer.cornerRadius = 13
+        
+        viewLabel.layer.cornerRadius = 13
+        getColorForViewLabel()
         
         redLabel.text = String(redSlider.value)
         greenLabel.text = String(greenSlider.value)
         blueLabel.text = String(blueSlider.value)
+        
     }
+    
+    func getColorForViewLabel() {
+        viewLabel.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
     @IBAction func redSliderAction() {
         redLabel.text = String(format: "%.2f", redSlider.value)
+        getColorForViewLabel()
     }
     
     @IBAction func greenSliderAction() {
         greenLabel.text = String(format: "%.2f", greenSlider.value)
+        getColorForViewLabel()
     }
     
     @IBAction func blueSliderAction() {
         blueLabel.text = String(format: "%.2f", blueSlider.value)
+        getColorForViewLabel()
     }
 }
 
