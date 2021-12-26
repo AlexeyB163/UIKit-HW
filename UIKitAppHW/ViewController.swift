8d4c1ac666c80a88dd3d673798b52591d325dafb
AlexeyB163 <sourceaisa@mail.ru>
2021-12-26 23:47:04 +0400

    added rounded value for slider

--- UIKitAppHW/ViewController.swift
+++ UIKitAppHW/ViewController.swift
@@ -11,12 +11,32 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var viewColor: UIView!
     
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewColor.layer.cornerRadius = 13
         
-        viewColor.layer.cornerRadius = 15
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
     }
-
-
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
