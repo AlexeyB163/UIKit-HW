cd0985790a28c88598b6b2e5d25982cad8c39968
AlexeyB163 <sourceaisa@mail.ru>
2021-12-26 23:16:36 +0400

    create UI

--- UIKitAppHW/ViewController.swift
+++ UIKitAppHW/ViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewColor.layer.cornerRadius = 15
     }
 
 
